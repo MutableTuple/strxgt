294bc230124bb087c29244f4344b31acab55921c
MutableTuple <mutabletuple39@gmail.com>
2024-11-05 16:47:20 +0530

    seo optimization

--- app/layout.js
+++ app/layout.js
@@ -12,25 +12,25 @@ export const metadata = {
   description:
     "Showcase your gaming achievements and skills with Strontium. Track your gaming stats, connect with friends, and explore the gaming community.",
   keywords:
-    "gaming portfolio, track gaming stats, gaming achievements, gamer community, gaming profile, connect with friends, Strontium gaming, video game portfolio, online gamer profile, showcase gaming skills, Steam integration, custom games, add custom games, multiplayer portfolio, gaming accomplishments, video game statistics, esport achievements, gaming network, game collection, game stats tracker, competitive gaming profile, game history tracker, share gaming highlights, gaming social media, gamers community, online gaming identity, create gaming resume, Strontium app, professional gaming profile, gamer connections, gaming trophies, gaming milestones",
+    "gaming portfolio, track gaming stats, gaming achievements, gamer community, gaming profile, connect with friends, Strontium gaming, video game portfolio, online gamer profile, showcase gaming skills, Steam integration, custom games, add custom games, multiplayer portfolio, gaming accomplishments, video game statistics, esport achievements, gaming network, game collection, game stats tracker, competitive gaming profile, game history tracker, share gaming highlights, gaming social media, gamers community, online gaming identity, create gaming resume, Strontium app, professional gaming profile, gamer connections, gaming trophies, gaming milestones, best gaming phone, gaming app download, free game download, pc gaming, gaming logo, gaming phone, gaming images, genshin impact, mobile gaming, gaming website, online game app, gaming computer pc",
   author: "Strontium GT",
-  robots: "index, follow", // Instruct search engines to index the page and follow links
+  robots: "index, follow",
   og: {
     title: "Strontium - Your Ultimate Gaming Portfolio",
     description:
       "Showcase your gaming achievements and skills with Strontium. Track your gaming stats, connect with friends, and explore the gaming community.",
-    url: "www.strontium.app", // Replace with the actual URL of your website
+    url: "www.strontium.app",
     image:
-      "https://raw.githubusercontent.com/MutableTuple/fm_images/refs/heads/main/game_screen_cheetos.png", // Replace with a relevant image URL
+      "https://raw.githubusercontent.com/MutableTuple/fm_images/refs/heads/main/game_screen_cheetos.png",
     type: "website",
   },
   twitter: {
-    card: "summary_large_image", // Use summary or summary_large_image for a larger preview
+    card: "summary_large_image",
     title: "Strontium - Your Ultimate Gaming Portfolio",
     description:
       "Showcase your gaming achievements and skills with Strontium. Track your gaming stats, connect with friends, and explore the gaming community.",
     image:
-      "https://raw.githubusercontent.com/MutableTuple/fm_images/refs/heads/main/game_screen_cheetos.png", // Replace with a relevant image URL
+      "https://raw.githubusercontent.com/MutableTuple/fm_images/refs/heads/main/game_screen_cheetos.png",
   },
 };
 export default function RootLayout({ children }) {
